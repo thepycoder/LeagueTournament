57e407ea47876092591f54c13c4fd768204d722e
stephan <stephan@sjappie.lan>
2015-11-11 13:44:37 +0100

    Standaard klassen

--- src/lol/Team.java
+++ src/lol/Team.java
@@ -5,10 +5,63 @@
  */
 package lol;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author stephan
  */
 public class Team {
+    private String name;
+    private String region;
+    private String official;
+    private ArrayList <String> members;
+
+    public Team ()
+    {
+    }
+    
+    public Team(String name, String region, String official, ArrayList<String> members) {
+        this.name = name;
+        this.region = region;
+        this.official = official;
+        this.members = members;
+    }
+
+    //getters
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getOfficial() {
+        return official;
+    }
+
+    public ArrayList<String> getMembers() {
+        return members;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setOfficial(String official) {
+        this.official = official;
+    }
+
+    public void setMembers(ArrayList<String> members) {
+        this.members = members;
+    }
+    
     
 }
