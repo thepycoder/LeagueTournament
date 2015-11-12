46d79f3a8aec7dc8a590e294167d6414ba7cb9c7
vsonck <vsonck@ATHA2207.UGent.be>
2015-11-12 18:47:27 +0100

    nieuwe methode om connection te openen

--- src/lol/DatabaseHandler.java
+++ src/lol/DatabaseHandler.java
@@ -5,10 +5,39 @@
  */
 package lol;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Temp
  */
 public class DatabaseHandler {
     
+    public String user = "BINFG16";
+    public String pass = "f9xff87y";
+    public String url = "mysqlha2.ugent.be";
+    
+    
+    public Connection createConnection(String url) {
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection conn = DriverManager.getConnection(url,user,pass);
+            
+            return conn;
+            
+        } catch (ClassNotFoundException ex) {
+            //Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("The jdbc driver was not found! " + ex);
+            return null;
+        } catch (SQLException ex) {
+            //Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("A connection to the databse could not be established! " + ex);
+            return null;
+        }
+        
+    }
 }
